bc7f4b4b270598ea0b564053bd89d4cf2461e666
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-01-15 19:17:31 +0000

    Factored out a couple of methods

--- DiamondLib/Diamond.cs
+++ DiamondLib/Diamond.cs
@@ -6,28 +6,21 @@ namespace DiamondLib
 {
     public class Diamond
     {
+        private const char BaseChar = 'A';
+
         public static IEnumerable<string> GenerateLines(char inputChar)
         {
-            const char baseChar = 'A';
-            var squareSize = ((inputChar - baseChar) * 2) + 1;
-            var increasingTuples = Enumerable.Range(0, squareSize / 2 + 1)
-                .Select(i => Tuple.Create(i * 2 + 1, Convert.ToChar(baseChar + i)))
-                .ToArray();
-            var decreasingTuples = increasingTuples.Reverse().Skip(1);
-            var tuples = increasingTuples.Concat(decreasingTuples);
-
+            var squareSize = CalculateSquareSize(inputChar);
             var lines = new List<string>();
 
-            foreach (var tuple in tuples)
+            foreach (var tuple in GenerateTuples(squareSize))
             {
                 var patternWidth = tuple.Item1;
                 var currentChar = tuple.Item2;
-                var numBeforeAndAfterSpaces = (squareSize - patternWidth) / 2;
-                var beforeAndAfterSpaces = numBeforeAndAfterSpaces.ToSpaces();
+                var beforeAndAfterSpaces = ((squareSize - patternWidth) / 2).ToSpaces();
                 if (patternWidth > 1)
                 {
-                    var numCentreSpaces = patternWidth - 2;
-                    var centreSpaces = numCentreSpaces.ToSpaces();
+                    var centreSpaces = (patternWidth - 2).ToSpaces();
                     lines.Add(string.Format("{0}{1}{2}{1}{0}", beforeAndAfterSpaces, currentChar, centreSpaces));
                 }
                 else
@@ -37,6 +30,20 @@ namespace DiamondLib
             }
 
             return lines;
+        }
+
+        private static int CalculateSquareSize(char inputChar)
+        {
+            return ((inputChar - BaseChar) * 2) + 1;
+        }
+
+        private static IEnumerable<Tuple<int, char>> GenerateTuples(int squareSize)
+        {
+            var increasingTuples = Enumerable.Range(0, squareSize / 2 + 1)
+                .Select(i => Tuple.Create(i * 2 + 1, Convert.ToChar(BaseChar + i)))
+                .ToArray();
+            var decreasingTuples = increasingTuples.Reverse().Skip(1);
+            return increasingTuples.Concat(decreasingTuples);
         }
     }
 }
